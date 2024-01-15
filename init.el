5926ad86e2931520bd082f85f37cac988bd02be8
Ryan Harvey <rharv99@gmail.com>
2024-01-15 15:06:58 -0500

    sync

--- init.el
+++ init.el
@@ -1,7 +1,6 @@
 ;; Open Eshell at startup
 (setq inhibit-startup-screen t) ; Disable the startup screen
-(term "/bin/bash")
-(setq confirm-kill-processes nil)
+(eshell)
 
 (defun run-shell-command-no-output (command)
   "Run shell COMMAND and display output in echo area."
