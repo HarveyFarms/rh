659f1c329ed91b2004dff72df005dfc3086279ad
Ryan Harvey <rharv99@gmail.com>
2024-01-19 12:10:13 -0500

    sync

--- init.el
+++ init.el
@@ -260,7 +260,7 @@
       (file "~/rh/org/templates/book.org")
       :immediate-finish t)) t)
  '(org-cycle-hide-block-startup t)
- '(org-cycle-inline-images-display nil)
+ '(org-cycle-inline-images-display t)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
  '(tooltip-mode nil))
