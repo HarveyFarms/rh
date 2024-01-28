0e8bea197cdce29e92d7e56882bfec7ff51b72cf
Ryan Harvey <rharv99@gmail.com>
2024-01-28 13:13:32 -0600

    sync

--- sdl/main.cpp
+++ sdl/main.cpp
@@ -1,61 +1,84 @@
 #include <SDL2/SDL.h>
 
-const int SCREEN_WIDTH = 1920;
-const int SCREEN_HEIGHT = 1080;
+const int WIN_X = 1920;
+const int WIN_Y = 1080;
 
-bool running = true;
+typedef struct 
+{
+  SDL_Window * window;
+  SDL_Renderer * renderer;
+  bool is_running;
+} App;
+App * app = new App;
+
+
+bool init();
+void prepare_scene();
+void handle_input();
+void present_scene();
+void clean();
 
 int main()
 {
-  SDL_Window * window = NULL;
-  SDL_Renderer * renderer = NULL;
-  // Initialize SDL
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    // Handle initialization error
-    return 1;
+  if (!init()) return 1;
+
+  while (app->is_running)
+  {
+    prepare_scene();
+
+    handle_input();
+
+    present_scene();
+
+    SDL_Delay(16);
   }
 
-  // Create window and renderer
-  window = SDL_CreateWindow("testing SDL ...", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-  renderer = SDL_CreateRenderer(window, -1, 0);
-
-  if (!window || !renderer) {
-    // Handle window or renderer creation error
-    SDL_Quit();
-    return 1;
-  }
-
-  SDL_Rect * myrect = new SDL_Rect{300, 200, 100, 200};
-
-  while (running) {
-    SDL_Event e;
-    while (SDL_PollEvent(&e) != 0)
-      {
-      if (e.type == SDL_QUIT)
-	running = false;
-      }
-
-    // Render
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, myrect);
-
-    SDL_RenderPresent(renderer);
-
-    // Delay (optional)
-    SDL_Delay(10);
-  }
-
-  // Clean up resources
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-
-  // Quit SDL subsystems
-  SDL_Delay(1000);
-  SDL_Quit();
+  clean();
 
   return 0;
 }
 
+bool init()
+{
+  if (SDL_Init(SDL_INIT_EVERYTHING) < 0) return false;
+  else 
+  {
+    app->window = SDL_CreateWindow("test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIN_X, WIN_Y, SDL_WINDOW_FULLSCREEN);
+    if (!app->window) return false;
+    else 
+    {
+      app->renderer = SDL_CreateRenderer(app->window, -1, 0);
+      if (!app->renderer) return false;
+    }
+  }
+  return true;
+}
+void prepare_scene()
+{
+  SDL_SetRenderDrawColor(app->renderer, 60, 60, 60, 150);
+  SDL_RenderClear(app->renderer);
+}
+void handle_input()
+{
+  SDL_Event event;
+  while (SDL_PollEvent(&event))
+  {
+    switch (event.type)
+    {
+      case SDL_QUIT:
+        app->is_running = false;
+        break;
+      default:
+        break;
+    }
+  }
+}
+void present_scene()
+{
+  SDL_RenderPresent(app->renderer);
+}
+
+void clean()
+{
+  delete app;
+}
