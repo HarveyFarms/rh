94d5625acf9dfa01ea2d3112748a0f3ac8db551d
Ryan Harvey <rharv99@gmail.com>
2024-01-30 21:32:40 -0500

    sync

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,4 +14,82 @@ return {
     },
     lazy = false,
   },
+  {
+    -- amongst your other plugins
+    { "akinsho/toggleterm.nvim", version = "*", config = true },
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon"):setup()
+    end,
+    keys = {
+      {
+        "<leader>j",
+        function()
+          require("harpoon"):list():append()
+        end,
+        desc = "harpoon file",
+      },
+      {
+        "<leader>h",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "harpoon quick menu",
+      },
+      {
+        "<leader>1",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "harpoon to #1",
+      },
+      {
+        "<leader>2",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "harpoon to #2",
+      },
+      {
+        "<leader>3",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "harpoon to #3",
+      },
+      {
+        "<leader>4",
+        function()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "harpoon to #4",
+      },
+      {
+        "<leader>5",
+        function()
+          require("harpoon"):list():select(5)
+        end,
+        desc = "harpoon to #5",
+      },
+      {
+        "<C-N>",
+        function()
+          require("harpoon"):list():next()
+        end,
+        desc = "harpoon next",
+      },
+      {
+        "<C-P>",
+        function()
+          require("harpoon"):list():prev()
+        end,
+        desc = "harpoon previous",
+      },
+    },
+  },
 }
