2cdbc4504f5288241d55b49e2b4a03b3039b8195
Ryan Harvey <rharv99@gmail.com>
2024-02-05 11:43:05 -0500

    sync

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map Ctrl-j to ":normal o" in normal mode
+vim.api.nvim_set_keymap("n", "<C-j>", ":normal o<CR>", { noremap = true, silent = true })
+
+-- Map Ctrl-J to ":normal O" in normal mode
+vim.api.nvim_set_keymap("n", "<C-k>", ":normal O<CR>", { noremap = true, silent = true })
