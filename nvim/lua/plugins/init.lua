608606ee552b24f0d5c38e9b3addd488f79dfa36
Ryan Harvey <rharv99@gmail.com>
2024-02-12 00:52:34 -0500

    sync

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -121,12 +121,5 @@ return {
   -- Lua
   {
     "folke/zen-mode.nvim",
-    config = {
-      window = {
-        backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-        width = 1, -- width of the Zen window
-        height = 1, -- height of the Zen window
-      },
-    },
   },
 }
