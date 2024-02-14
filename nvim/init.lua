e803ff2ae7d4cda6bbc24551ed5f8cabde3e9d06
Ryan Harvey <rharv99@gmail.com>
2024-02-13 21:49:25 -0500

    sync

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.api.nvim_command(
+  "autocmd VimEnter * lua vim.defer_fn(function() vim.api.nvim_command('Neorg workspace home') end, 0)"
+)
+vim.api.nvim_command(
+  "autocmd VimEnter * lua vim.defer_fn(function() vim.api.nvim_command('Neorg journal today') end, 0)"
+)
