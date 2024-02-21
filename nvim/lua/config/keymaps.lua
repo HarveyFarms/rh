15fb4ee25e88a1a1000609e90389a5f4dd06b323
Ryan Harvey <rharv99@gmail.com>
2024-02-21 10:42:19 -0500

    sync

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -20,4 +20,4 @@ vim.api.nvim_set_keymap(
 -- for workspaces in neorg
 vim.api.nvim_set_keymap("n", "<leader>wh", ":Neorg workspace home<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ws", ":Neorg workspace school<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fq", ":Calendar -view=month<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fq", ":Calendar -view=month <CR>", { noremap = true, silent = true })
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,10 +7,11 @@
 
 return {
   -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+  { "Mofiqul/dracula.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "habamax",
+      colorscheme = "dracula",
     },
   },
   {
