c410d82ac6c94e3bb3611e9d544412997e0a88c9
Ryan Harvey <rharv99@gmail.com>
2024-03-25 19:25:56 -0400

    sync

--- doom/custom.el
+++ doom/custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(doom-gruvbox))
+ '(custom-safe-themes
+   '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files '("~/rh/org-agenda"))
  '(org-cycle-hide-block-startup t)
@@ -20,7 +23,8 @@
      (gnus . org-gnus-no-new-news)
      (file . find-file)
      (wl . wl-other-frame)))
- '(org-startup-folded 'show2levels)
+ '(org-startup-folded t)
+ '(org-startup-with-inline-images t)
  '(package-selected-packages
    '(org-gcal calfw-org calfw org-roam-bibtex org-pomodoro org-roam-ui org-roam-timestamps org-roam-ql-ql org-roam-ql org-roam)))
 (custom-set-faces
